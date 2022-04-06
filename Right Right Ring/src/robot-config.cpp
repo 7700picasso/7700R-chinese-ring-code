2411953459226c614c44acde6f6d05d0729de67d
KingCuddles314 <48599511+KingCuddles314@users.noreply.github.com>
2022-04-05 19:07:34 -0700

    reordered devices

--- Right Right Ring/src/robot-config.cpp
+++ Right Right Ring/src/robot-config.cpp
@@ -10,21 +10,21 @@ brain  Brain;
 // VEXcode device constructors
 controller Controller1 = controller(primary);
 motor leftDrive1 = motor(PORT4, ratio18_1, true);
-motor leftDrive2 = motor(PORT6, ratio18_1, true);
 motor leftmiddle = motor(PORT5, ratio18_1, false);
+motor leftDrive2 = motor(PORT6, ratio18_1, true);
 motor rightDrive1 = motor(PORT1, ratio18_1, false);
-motor rightDrive2 = motor(PORT3, ratio18_1, true);
 motor rightmiddle = motor(PORT2, ratio18_1, false);
+motor rightDrive2 = motor(PORT3, ratio18_1, true);
 motor Lift = motor(PORT9, ratio36_1, false);
-inertial Gyro = inertial(PORT19);
+motor Rings = motor(PORT10, ratio6_1, true);
+digital_out claw1 = digital_out(Brain.ThreeWirePort.E);
+digital_out MogoTilt = digital_out(Brain.ThreeWirePort.C);
+digital_out Transmission = digital_out(Brain.ThreeWirePort.F);
 gps GPS = gps(PORT8, 220.00, 50.00, mm, 90);
+inertial Gyro = inertial(PORT19);
 distance DistFront = distance(PORT15);
 distance DistBack = distance(PORT16);
 distance DistClaw = distance(PORT17);
-digital_out MogoTilt = digital_out(Brain.ThreeWirePort.C);
-digital_out Transmission = digital_out(Brain.ThreeWirePort.F);
-motor Rings = motor(PORT10, ratio6_1, true);
-digital_out claw1 = digital_out(Brain.ThreeWirePort.E);
 
 // VEXcode generated functions
 // define variable for remote controller enable/disable
